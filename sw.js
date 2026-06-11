5666c995b97f7a0a26e49eccd57693132d118076
100jungeu-ops <100jungeu@gmail.com>
2026-06-11 22:01:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='vino-quest-v1';
+const C='vino-quest-v2';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
   self.skipWaiting();
@@ -8,9 +8,20 @@ self.addEventListener('activate',e=>{
   self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      const cl=res.clone();caches.open(C).then(c=>c.put(e.request,cl));return res;
-    }).catch(()=>caches.match('./index.html')))
-  );
+  const req=e.request;
+  const isHTML=req.mode==='navigate'||(req.headers.get('accept')||'').includes('text/html');
+  if(isHTML){
+    // 네트워크 우선: 항상 최신 버전, 오프라인일 때만 캐시
+    e.respondWith(
+      fetch(req).then(res=>{
+        const cl=res.clone();caches.open(C).then(c=>c.put(req,cl));return res;
+      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    );
+  }else{
+    e.respondWith(
+      caches.match(req).then(r=>r||fetch(req).then(res=>{
+        const cl=res.clone();caches.open(C).then(c=>c.put(req,cl));return res;
+      }))
+    );
+  }
 });
